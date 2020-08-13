282f577bcb9a0132b752340a33886351b3318740
Daniil <pde-rnd@yandex.ru>
2020-08-13 21:19:41 +0300

    Изменено: Необходимые react-компонентам функции хранятся в объекте-контейнере Индикатор может принимать необязательный параметр размера (css-класс)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Pantograph from './Machine/Pantograph';
 import Group from './Group/Group';
+import Switcher from './Switcher/Switcher';
+import Indicator from './Indicator/Indicator';
+
+enum State {
+  stopped, starting, started
+}
 
 function App() {
+  const Pantograph = {
+    val: useState(0) as [0 | 1 |2, any],
+    get() { return this.val[0] },
+    toString() { return State[this.val[0]] },
+    set(newVal: 0 | 1) {
+      Pantograph.val[1](State.starting);
+      setTimeout(() => {
+        Pantograph.val[1](newVal === 1 ? State.started : State.stopped)
+      }, 2000);
+    }
+  }
   return (
     <>
-    <Group className="left" label="Токоприёмники">
-      <Pantograph label="Передний" />
-    </Group>
-    <Group label="Токоприёмники" className="right" id="indicators"></Group>
+      <Group className="left" label="Токоприёмники">
+        <Switcher descrOn="Поднять" descrOff="Опустить" initVal={0} label="Передний" onSwitch={Pantograph.set} />
+      </Group>
+      <Group label="Токоприёмники" className="right" id="indicators">
+        <Indicator val={Pantograph.toString()} />
+      </Group>
     </>
   );
 }
--- src/Indicator/Indicator.tsx
+++ src/Indicator/Indicator.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Indicator.css'
 
-const Indicator = (props: {val: string, label?: string}) => {
+const Indicator = (props: {val: string, label?: string, size?:string}) => {
     return (
         <React.Fragment>
-            <div className={`indicator ${props.val}`}/>
+            <div className={`indicator ${props.val} ${props.size ? props.size : "small"}`}/>
             <div className="label">{props.label}</div>
         </React.Fragment>
     )
