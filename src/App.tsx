f4e62a2824df4f313f243123b58e90ba3c7477b3
Daniil <pde-rnd@yandex.ru>
2020-08-14 17:10:11 +0300

    Создан класс Pantograph

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,46 @@ import Group from './Group/Group';
 import Switcher from './Switcher/Switcher';
 import Indicator from './Indicator/Indicator';
 
-enum State {
+enum ComponentState {
   stopped, starting, started
+}
+enum SwitchState {
+  off,on
+}
+class Pantograph {
+  private val: [ComponentState, any]
+  constructor(val = 0 as ComponentState) {
+    this.val = useState(val) as [ComponentState, any];
+  }
+  get(): ComponentState {
+    return this.val[0]
+  }
+  set(newVal: SwitchState): void {
+    this.val[1](ComponentState.starting);
+    setTimeout(() => {
+      this.val[1](newVal === 1 ? ComponentState.started : ComponentState.stopped)
+    }, 2000);
+  }
+  toString(): string {
+    return ComponentState[this.get()]
+  }
+  toSwitch(): SwitchState {
+    return this.get() === 0 ? 0 : 1;
+  }
 }
 
 function App() {
-  const PantographFront = {
-    val: useState(0) as [0 | 1 |2, any],
-    get() { return this.val[0] },
-    toString() { return State[this.val[0]] },
-    set(newVal: 0 | 1) {
-      PantographFront.val[1](State.starting);
-      setTimeout(() => {
-        PantographFront.val[1](newVal === 1 ? State.started : State.stopped)
-      }, 2000);
-    }
-  }
-  const PantographBack = {
-    val: useState(0) as [0 | 1 |2, any],
-    get() { return this.val[0] },
-    toString() { return State[this.val[0]] },
-    set(newVal: 0 | 1) {
-      PantographBack.val[1](State.starting);
-      setTimeout(() => {
-        PantographBack.val[1](newVal === 1 ? State.started : State.stopped)
-      }, 2000);
-    }
-  }
+  const PantographFront = new Pantograph(0);
+  const PantographBack = new Pantograph(2);
   return (
     <>
       <Group className="left" label="Токоприёмники">
-        <Switcher descrOn="Поднять" descrOff="Опустить" initVal={0} label="Передний" onSwitch={PantographFront.set} />
-        <Switcher descrOn="Поднять" descrOff="Опустить" initVal={0} label="Задний" onSwitch={PantographBack.set} />
+        <Switcher descrOn="Поднять" descrOff="Опустить" initVal={PantographFront.toSwitch()} label="Передний" onSwitch={PantographFront.set.bind(PantographFront)} />
+        <Switcher descrOn="Поднять" descrOff="Опустить" initVal={PantographBack.toSwitch()} label="Задний" onSwitch={PantographBack.set.bind(PantographBack)} />
       </Group>
       <Group label="Токоприёмники" className="right" id="indicators">
-        <Indicator val={PantographFront.toString()} label="Передний"/>
-        <Indicator val={PantographBack.toString()} label="Задний"/>
+        <Indicator val={PantographFront.toString()} label="Передний" />
+        <Indicator val={PantographBack.toString()} label="Задний" />
       </Group>
     </>
   );
